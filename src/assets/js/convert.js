0b8fc37983b775c63f85fc790aefd7cdc71ffcce
sivacchi <hiroki.kh@gmail.com>
2017-06-18 18:49:33 +0900

    add line break convart

--- src/assets/js/convert.js
+++ src/assets/js/convert.js
@@ -2,10 +2,19 @@ $(function() {
 	$('#btn-convert').on('click', function() {
 		let str = $('#before').val();
 		str = str
+            // .replace(/\t{1,}- (.*)/gmi,"-- $1")
 			.replace(/\[(.*)\]\((.*)\)/g, "[[$1:$2]]")
-			.replace(/\`\`\`([\s\S]*?)\`\`\`\n/g, "{code}$1{/code}")
-			.replace(/^###/gmi, '***')
-			.replace(/^##/gmi, '**');
+			.replace(/\`\`\`\n([\s\S]*?)\`\`\`\n/g, "{code}$1{/code}")
+			.replace(/\`\`\`(java|cs)\n([\s\S]*?)\`\`\`\n/g, "{code:$1}\n$2{/code}")
+			.replace(/\n^######/gmi, '******')
+			.replace(/\n^#####/gmi, '*****')
+			.replace(/\n^####/gmi, '****')
+			.replace(/\n^###\s?(.*)\n\n?$/gmi, '*** $1')
+			.replace(/\n^##\s?(.*)\n\n?$/gmi, '** $1')
+			.replace(/\n^#/gmi, '*')
+            .replace("\n{{TOC}}\n","#contents");
+        str = str
+            .replace(/^--{2,}\n/gmi,'');
 		$('#after').html(str);
 	})
 });
